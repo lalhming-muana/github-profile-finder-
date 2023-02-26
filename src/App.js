16948f07802993d124f7190f2066ba877082756c
“Muana” <lalhming.muana1982@gmail.com>
2023-02-26 11:23:18 +0530

    JSX and fragments

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 
 class App extends React.Component {
 
   render(){
     return (
-      <div className="App">
+      <Fragment>
         <h1>Hello from react</h1>
-      </div>
+        <label htmlFor="name">Name</label>
+        <h2>Goodbye</h2>
+      </Fragment>
+     
     );
 
   }
